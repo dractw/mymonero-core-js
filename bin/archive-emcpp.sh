c2ee6f8c9f02b4cb7894f22146ea02e8e1377aa7
Сковорода Никита Андреевич <chalkerx@gmail.com>
2020-12-28 20:08:55 +0300

    build: remove ASMJS files (#27)

--- bin/archive-emcpp.sh
+++ bin/archive-emcpp.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
+rm -f monero_utils/MyMoneroCoreCpp_*
 bin/build-emcpp.sh &&
 cp build/MyMoneroCoreCpp_WASM.js monero_utils/; 
 cp build/MyMoneroCoreCpp_WASM.wasm monero_utils/;
-cp build/MyMoneroCoreCpp_ASMJS.js monero_utils/; 
-cp build/MyMoneroCoreCpp_ASMJS.asm.js monero_utils/ 
