2ac22077ff03b081b83ef1b6b5608f05a55adf02
dractw <dractw@gmail.com>
2021-01-07 13:01:52 +0300

    build: parallelize build by cpu count (#32)
    (cherry picked from commit 69630f472c7f6a3f2b9a07f99ccda349b8edc272)

--- bin/archive-emcpp.sh
+++ bin/archive-emcpp.sh
@@ -4,3 +4,5 @@ rm -f monero_utils/MyMoneroCoreCpp_*
 bin/build-emcpp.sh &&
 cp build/MyMoneroCoreCpp_WASM.js monero_utils/; 
 cp build/MyMoneroCoreCpp_WASM.wasm monero_utils/;
+cp build/MyMoneroCoreCpp_ASMJS.js monero_utils/;
+cp build/MyMoneroCoreCpp_ASMJS.asm.js monero_utils/ 
